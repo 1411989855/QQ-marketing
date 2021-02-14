4367604ce4ceca784a761c09c16ae8aaca960d27
jia110 <38829156+jia110@users.noreply.github.com>
2021-02-14 08:51:29 +0800

    Update qw.py

--- qw.py
+++ qw.py
@@ -9,7 +9,7 @@ headers = {
   'q-qimei': 'a9bb0b00442266bc10867c98800010515105',
   'qimei36': 'b37e2cea524ae6d1468c567f10001ff15113',
   'q-auth': '31045b957cf33acf31e40be2f3e71c5217597676a9725f1b',
-  'referer': 'https://club.vip.qq.com/card/friend?_wv=16778247&_wwv=68&_wvx=10&_proxy=1&_proxyByURL=1&platform=1&qq=767026787&adtag=geren&aid=mvip.pingtai.mobileqq.androidziliaoka.fromqita',
+  'referer': '*********=16778247&_wwv=68&_wvx=10&_proxy=1&_proxyByURL=1&platform=1&qq=767026787&adtag=geren&aid=mvip.pingtai.mobileqq.androidziliaoka.fromqita',
   'Cookie': 'pvid=7392286914; uin=o2582699514; p_uin=o2582699514; RK=HbTNpSB6Xp; ptcz=5542752676d055ad3b7397db42dfab0970b43b52e0de434c1c0730cc97440b26; skey=MHVjboSqey; pt4_token=PPoEGHPhYG-yNsDkQQ3gMnBRH9DZQrw9nA5fVATUTco_; p_skey=MWRnkYqg-Q1HPo0RKg1QY7lrd2rjEWogNq94pb1SDYo_'
 }
 
@@ -19,7 +19,7 @@ qq.close()
 new_f = open('./data.txt','w')
 i =1
 for k in qqlist.split("\n"):
-    url = "https://cgi.vip.qq.com/card/getExpertInfo?ps_tk=397022032&fuin=%s&callback=jsonp2" % str(k)
+    url = "********" % str(k)
     try:
         response = requests.request("GET", url, headers=headers, data=payload)
         #print(url,response)
